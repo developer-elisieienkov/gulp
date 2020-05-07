85f8d13e8c08ac3a4987f58ba694b10ce05fc5b0
bladbird <eliseenkovandrej@gmail.com>
2020-05-07 21:32:20 +0300

    add

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,68 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    browse = require('browser-sync').create(),
-    autopref = require('gulp-autoprefixer'),
-    postcss = require('gulp-postcss'),
-    cssmin = require('gulp-cssmin'),
-    rename = require('gulp-rename'),
-    babel = require('gulp-babel');
-function scssToCss() {
-       return  gulp.src('./src/scss/style.scss')
-            .pipe(sass({
-                errorLogToConsole: true
-            }))
-            .pipe(autopref({
-                overrideBrowserslist: ['last 15 versions', '> 1%','ie 11','ie 8', 'ie 7'],
-                cascade: false
-            }))
-            .pipe(cssmin())
-            .pipe(rename({suffix: '.min'}))
-            .on('error',console.error.bind(console))
-            .pipe( gulp.dest('./dist/'))
-            .pipe(browse.stream())
-
+var gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  browse = require("browser-sync").create(),
+  autopref = require("gulp-autoprefixer"),
+  postcss = require("gulp-postcss"),
+  cssmin = require("gulp-cssmin"),
+  rename = require("gulp-rename"),
+  babel = require("gulp-babel");
+function scssToCss(done) {
+  return gulp
+    .src("./src/scss/style.scss")
+    .pipe(
+      sass({
+        errorLogToConsole: true,
+      })
+    )
+    .pipe(
+      autopref({
+        overrideBrowserslist: [
+          "last 15 versions",
+          "> 1%",
+          "ie 11",
+          "ie 8",
+          "ie 7",
+        ],
+        cascade: false,
+      })
+    )
+    .pipe(cssmin())
+    .pipe(rename({ suffix: ".min" }))
+    .on("error", console.error.bind(console))
+    .pipe(gulp.dest("./dist/"))
+    .pipe(browse.stream());
+  done();
 }
 
-function babelator() {
-    gulp.src('src/js/script.js')
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(gulp.dest('dist'))
-        .pipe(browse.stream())
+function babelator(done) {
+  gulp
+    .src("src/js/script.js")
+    .pipe(
+      babel({
+        presets: ["@babel/env"],
+      })
+    )
+    .pipe(gulp.dest("dist"))
+    .pipe(browse.stream());
+  done();
 }
 
-function browserSync(){
-    browse.init({
-        server:{
-            baseDir:"./dist"
-        }
-    });
+function browserSync(done) {
+  browse.init({
+    server: {
+      baseDir: "./dist",
+    },
+  });
+  done();
 }
-function browserReboot(){
-    browse.reload({streem:true});
+function browserReboot(done) {
+  browse.reload({ streem: true });
+  done();
 }
-function sharingan(){
-    gulp.watch("./src/scss/*", scssToCss);
-    gulp.watch("./dist/*.html", browserReboot);
-    gulp.watch("./src/js/*", babelator);
+function sharingan(done) {
+  gulp.watch("./src/scss/*", scssToCss);
+  gulp.watch("./dist/*.html", browserReboot);
+  gulp.watch("./src/js/*", babelator);
+  done();
 }
-gulp.task('default',gulp.parallel(browserSync,sharingan));
+gulp.task("default", gulp.parallel(browserSync, sharingan));
